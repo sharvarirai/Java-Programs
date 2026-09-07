d7dbe62c2210597fd3d605818721dd0abc4272d3
Sharvari Rai <raisharvari946@gmail.com>
2026-09-07 19:29:42 +0530

    String program 1

---
+++ Module3/string.java
@@ -0,0 +1,14 @@
+
+public class string {
+
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "Java";
+        if (s1 == s2) {
+            System.out.println("Ref are equal");
+        } else {
+            System.out.println("Ref are not equal");
+        }
+    }
+
+}
